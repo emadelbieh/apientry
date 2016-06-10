5a6ddb0ffc66834d7425634769a0fcf69da5b80b
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-06-10 19:09:24 +0800

    Adds bootstrap, Sass, and postcss

--- brunch-config.js
+++ brunch-config.js
@@ -3,22 +3,6 @@ exports.config = {
   files: {
     javascripts: {
       joinTo: "js/app.js"
-
-      // To use a separate vendor.js bundle, specify two files path
-      // https://github.com/brunch/brunch/blob/stable/docs/config.md#files
-      // joinTo: {
-      //  "js/app.js": /^(web\/static\/js)/,
-      //  "js/vendor.js": /^(web\/static\/vendor)|(deps)/
-      // }
-      //
-      // To change the order of concatenation of files, explicitly mention here
-      // https://github.com/brunch/brunch/tree/master/docs#concatenation
-      // order: {
-      //   before: [
-      //     "web/static/vendor/js/jquery-2.1.1.js",
-      //     "web/static/vendor/js/bootstrap.min.js"
-      //   ]
-      // }
     },
     stylesheets: {
       joinTo: "css/app.css"
@@ -30,28 +14,26 @@ exports.config = {
 
   conventions: {
     // This option sets where we should place non-css and non-js assets in.
-    // By default, we set this to "/web/static/assets". Files in this directory
-    // will be copied to `paths.public`, which is "priv/static" by default.
     assets: /^(web\/static\/assets)/
   },
 
-  // Phoenix paths configuration
   paths: {
-    // Dependencies and current project directories to watch
     watched: [
       "web/static",
       "test/static"
     ],
 
-    // Where to compile files to
-    public: "priv/static"
+    public: "priv/static" // Where to compile files to
   },
 
-  // Configure your plugins
   plugins: {
     babel: {
-      // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
+    },
+    postcss: {
+      processors: [
+        require('autoprefixer')()
+      ]
     }
   },
 
@@ -65,6 +47,9 @@ exports.config = {
     enabled: true,
     // Whitelist the npm deps to be pulled in as front-end assets.
     // All other deps in package.json will be excluded from the bundle.
-    whitelist: ["phoenix", "phoenix_html"]
+    whitelist: [
+      "phoenix",
+      "phoenix_html"
+    ]
   }
 };
