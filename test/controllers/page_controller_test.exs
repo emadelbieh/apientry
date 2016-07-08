9f2257929f61eaefbca57e10d8cbf048de6ce1f7
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-07-08 17:37:32 +0800

    Fixes tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Apientry.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "api entry"
+    assert html_response(conn, 200) =~ "<html"
   end
 end
