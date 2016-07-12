2daf958165499b062c0ed203209ceb5017156002
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-07-12 14:19:01 +0800

    Fixes phoenix_html form submit behavior

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,13 +1,23 @@
-import 'phoenix_html'
-
 // Set up 3rd-party libs (don't use `import`)
 window.jQuery = window.$ = require('jquery')
 window.onmount = require('onmount')
 require('bootstrap-sass')
-require('phoenix_html')
 
-$.onmount('[data-toggle="dropdown"]', function () {
+onmount('[data-toggle="dropdown"]', function () {
   $(this).dropdown()
 })
 
 $(function () { onmount() })
+
+/*
+ * jQuery implementation of phoenix_html, because for some reason
+ * phoenix_html doesn't play well with Bootstrap sass.
+ */
+
+$(document).on('click', 'a[data-submit="parent"]', function (e) {
+  e.preventDefault()
+  var message = this.getAttribute('data-confirm')
+  if (message === null || confirm(message)) {
+    this.parentNode.submit()
+  }
+})
