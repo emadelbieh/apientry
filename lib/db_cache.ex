caa576c4dfcd6a55bdfa0f6f0d09eb1edeb19868
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-07-14 18:27:30 +0800

    Implements DbCache

---
+++ lib/db_cache.ex
@@ -0,0 +1,110 @@
+defmodule DbCache do
+  @moduledoc """
+  Database caching.
+
+      {:ok, pid} = DbCache.start_link(
+        repo: Apientry.Repo,
+        query: Apientry.Feed,
+        indices: [
+          country: &(&1.country),
+          country_mobile: &({&1.country_code, &1.is_mobile})
+        ])
+
+      # fetches everything
+      DbCache.fetch(pid)
+
+      # Find one
+      DbCache.lookup(pid, :country_mobile, {"US", true})
+
+      # Find many
+      DbCache.lookup_all(pid, :country, "US")
+  """
+
+  use GenServer
+
+  @doc """
+  Returns `{:ok, pid}`.
+  """
+  def start_link(opts \\ []) do
+    options = Enum.into(opts, %{})
+
+    unless options[:repo] && options[:query] && options[:indices] do
+      raise """
+      Invalid options
+
+      Required options: :repo, :query, :indices
+      """
+    end
+
+    with {:ok, pid} <- GenServer.start_link(__MODULE__, options) do
+      GenServer.call(pid, :init)
+      {:ok, pid}
+    end
+  end
+
+  @doc """
+  Fetches database.
+  """
+  def fetch(pid) do
+    GenServer.call(pid, :fetch)
+  end
+
+  @doc """
+  Lookup.
+  """
+  def lookup(pid, index, value) do
+    case lookup_all(pid, index, value) do
+      [record] -> record
+      [record | _] -> record
+      [] -> nil
+    end
+  end
+
+  @doc """
+  Lookup many.
+  """
+  def lookup_all(pid, index, value) do
+    GenServer.call(pid, {:lookup, index, value})
+  end
+
+  @doc """
+  Callback for GenServer.
+  """
+  def handle_call(:init, _, %{indices: indices} = state) do
+    tables = Enum.reduce(indices, %{}, fn {index, _fun}, map ->
+      table = :ets.new(:"db_cache_#{index}", [:bag])
+      Map.put(map, index, table)
+    end)
+
+    state = state
+    |> Map.put(:tables, tables)
+
+    {:reply, :ok, state}
+  end
+
+  def handle_call(:debug, _, state) do
+    {:reply, state, state}
+  end
+
+  def handle_call({:lookup, index, value}, _, %{tables: tables} = state) do
+    table = tables[index]
+    results = :ets.lookup(table, value) # [{"12", %Model}, ...]
+    objects = Enum.map(results, fn {_, obj} -> obj end)
+    {:reply, objects, state}
+  end
+
+  def handle_call(:fetch, _, %{repo: repo, query: query, tables: tables, indices: indices} = state) do
+    results = repo.all(query)
+
+    for {index, index_fun} <- indices do
+      table = tables[index]
+      :ets.delete_all_objects(table)
+      for record <- results do
+        key = index_fun.(record)
+        :ets.insert(table, {key, record})
+      end
+    end
+
+    {:reply, :ok, state}
+  end
+end
---
+++ test/services/db_cache_test.exs
@@ -0,0 +1,47 @@
+defmodule Apientry.DbCacheTest do
+  use Apientry.ModelCase
+
+  alias Apientry.Repo
+  alias Apientry.Feed
+
+  describe "database cache" do
+    setup [:mock_feeds, :start_cache]
+
+    test "lookup_all (feed type)", %{pid: pid} do
+      DbCache.fetch(pid)
+      result = DbCache.lookup_all(pid, :feed_type, "ebay")
+      assert length(result) == 6
+    end
+
+    test "lookup_all (type country mobile)", %{pid: pid} do
+      DbCache.fetch(pid)
+      [result] = DbCache.lookup_all(pid, :type_country_mobile, {"ebay", "US", true})
+      assert result.__struct__ == Feed
+      assert result.feed_type == "ebay"
+      assert result.country_code == "US"
+      assert result.is_mobile == true
+    end
+  end
+
+  defp mock_feeds(_context) do
+    Repo.insert!(%Feed{feed_type: "ebay", api_key: "us-d", is_active: true, is_mobile: false, country_code: "US"})
+    Repo.insert!(%Feed{feed_type: "ebay", api_key: "us-m", is_active: true, is_mobile: true,  country_code: "US"})
+    Repo.insert!(%Feed{feed_type: "ebay", api_key: "gb-d", is_active: true, is_mobile: false, country_code: "GB"})
+    Repo.insert!(%Feed{feed_type: "ebay", api_key: "gb-m", is_active: true, is_mobile: true,  country_code: "GB"})
+    Repo.insert!(%Feed{feed_type: "ebay", api_key: "au-d", is_active: true, is_mobile: false, country_code: "AU"})
+    Repo.insert!(%Feed{feed_type: "ebay", api_key: "au-m", is_active: true, is_mobile: true,  country_code: "AU"})
+    :ok
+  end
+
+  defp start_cache(_context) do
+    {:ok, pid} = DbCache.start_link(
+      repo: Repo,
+      query: Feed,
+      indices: [
+        feed_type: &(&1.feed_type),
+        type_country_mobile: &({&1.feed_type, &1.country_code, &1.is_mobile})
+      ])
+
+    {:ok, pid: pid}
+  end
+end
