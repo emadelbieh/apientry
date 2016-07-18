f6013c5eda39ef9f73819ddc0488014427df9760
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-07-18 14:40:36 +0800

    Validates event names in /redirect

--- test/controllers/redirect_controller_test.exs
+++ test/controllers/redirect_controller_test.exs
@@ -2,8 +2,13 @@ defmodule Apientry.RedirectControllerTest do
   use Apientry.ConnCase
 
   test "redirects", %{conn: conn} do
-    conn = get conn, redirect_path(conn, :show, Base.url_encode64("?link=http://google.com"))
+    conn = get conn, redirect_path(conn, :show, Base.url_encode64("?link=http://google.com&event=CLICK_OFFER_URL"))
     assert redirected_to(conn) == "http://google.com"
+  end
+
+  test "bad request (invalid event name)", %{conn: conn} do
+    conn = get conn, redirect_path(conn, :show, Base.url_encode64("?link=http://google.com&event=xyz"))
+    assert json_response(conn, 400)["error"] == "invalid_event"
   end
 
   test "bad request (no ?)", %{conn: conn} do
