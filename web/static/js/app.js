50f68e160d9a78e5ddeec0eec71328afa34ac3f6
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-07-21 18:59:55 +0800

    Improves the 'try this' section

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -21,3 +21,87 @@ $(document).on('click', 'a[data-submit="parent"]', function (e) {
     this.parentNode.submit()
   }
 })
+
+/*
+ * .try-this -- for the publishers
+ */
+
+!(function () {
+  onmount('[role~="try-this"]', function () {
+    var $this = $(this)
+
+    var $trackingId = $('[role~="trackingid"]', this)
+    var $ipAddress = $('[role~="ipaddress"]', this)
+    var $keyword = $('[role~="keyword"]', this)
+    var $text = $('[role~="text"]', this)
+    var $userAgent = $('[role~="useragent"]', this)
+    var $as = $('[role~="as"]', this)
+
+    var apiKey = $(this).attr('data-apikey')
+
+    update()
+
+    $(this).on('change', 'select', update)
+    $(this).on('input', 'input[type="text"]', update)
+
+    function update () {
+      $text.val(build({
+        apiKey: apiKey,
+        ipAddress: $ipAddress.val(),
+        userAgent: $userAgent.val(),
+        trackingId: $trackingId.val(),
+        keyword: $keyword.val(),
+        as: $as.val()
+      }))
+    }
+  })
+
+  /*
+   * Builds a test string
+   */
+
+  function build (options) {
+    var base = ''
+      + window.location.protocol
+      + '//'
+      + window.location.hostname
+
+    var query = queryString({
+      apiKey: options.apiKey,
+      keyword: options.keyword,
+      visitorIPAddress: options.ipAddress,
+      visitorUserAgent: options.userAgent,
+      domain: 'shopping.com',
+      trackingId: options.trackingId
+    })
+
+    if (options.as === 'curl') {
+      var url = base + '/publisher?' + query
+      return 'curl ' + JSON.stringify(url)
+    } else if (options.as === 'curl-dryrun') {
+      var url = base + '/dryrun/publisher?' + query
+      return 'curl ' + JSON.stringify(url) + ' -u admin'
+    } else {
+      var url = base + '/publisher?' + query
+      return url
+    }
+  }
+
+  /*
+   * Joins an object into a query string
+   */
+
+  function queryString (obj) {
+    var parts = Object.keys(obj).map(function (key) {
+      var val = obj[key]
+      if (val) {
+        return window.encodeURIComponent(key)
+          + '=' + window.encodeURIComponent(val)
+      } else {
+        return ''
+      }
+    })
+
+    return parts.join('&')
+  }
+}())
