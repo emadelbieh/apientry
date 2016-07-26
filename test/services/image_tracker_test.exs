3726e53f6e245a4a4575a3a66a3e3a136ee2005e
KL <git@kennethcalamay.com>
2016-07-26 22:29:59 +0800

    Fixes method name in test

--- test/services/image_tracker_test.exs
+++ test/services/image_tracker_test.exs
@@ -1,7 +1,7 @@
 defmodule Apientry.ImageTrackerTest do
   use ExUnit.Case, async: true
 
-  test "delete_unwanted removes unwanted kv pair from a map" do
+  test "remove_keys removes unwanted kv pair from a map" do
     input  = %{"a": "a", "b": "b", "c": "c", "d": "d"}
     output = %{c: "c", d: "d"}
 
