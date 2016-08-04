db8e851ab9f0b87a68d1287a96c8f6c088bbc71d
KL <git@kennethcalamay.com>
2016-08-04 14:25:37 +0800

    Provision for users to use 'endpoint' as a query param

--- web/controllers/search_controller.ex
+++ web/controllers/search_controller.ex
@@ -5,6 +5,8 @@ defmodule Apientry.SearchController do
       GET /publisher?keyword=nikon
   """
 
+  @default_endpoint "GeneralSearch"
+
   use Apientry.Web, :controller
 
   alias HTTPoison.Response
@@ -80,10 +82,10 @@ defmodule Apientry.SearchController do
   def set_search_options(%{query_string: query_string} = conn, _) do
     params = query_string
     |> StringKeyword.from_query_string()
-    |> StringKeyword.put("endpoint", conn.params["endpoint"])
 
     format = get_format(conn)
-    result = Searcher.search(format, params, conn)
+    endpoint = conn.params["endpoint"] || @default_endpoint
+    result = Searcher.search(format, endpoint, params, conn)
 
     result
     |> Enum.reduce(conn, fn {key, val}, conn -> assign(conn, key, val) end)
