48bba6ad9e634e8a11a0eee9d319e2b707306830
KL <git@kennethcalamay.com>
2016-09-05 17:06:05 +0800

    Code for tracking problematic code to rollbar

--- web/services/image_tracker.ex
+++ web/services/image_tracker.ex
@@ -1,4 +1,38 @@
 defmodule Apientry.ImageTracker do
+  def track_anomalous_images(body) do
+    case get_image_urls(body) do
+      {:ok, images} ->
+        for image_url < images do
+          # TODO: extract
+          Task.start(fn ->
+            {:ok, data} = HTTPoison.get(image_url)
+            status = data.status_code
+            headers = data.headers |> Enum.into(%{})
+            content_length = headers["Content-Length"] |> String.to_integer
+            unless status in 200..299 && content_length > 0 do
+              # TODO: extract / use ErrorReporter module
+              Task.start(fn ->
+                payload = %{
+                  "access_token": "",
+                  "data" : {
+                    "environment": "",
+                    "body": {
+                      "message": {
+                        "body": "Status is not in 200..299 or Content-Length is less than 1",
+                        "url": image_url
+                      }
+                    }
+                  }
+                }
+              end)
+            end
+          end)
+        end
+      _ ->
+        :error
+    end
+  end
+
   def get_image_urls(body) do
     body
     |> extract_categories
