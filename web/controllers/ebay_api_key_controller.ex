2ba701580fcd89ed30cd682ac70d7422616b6bda
KL <git@kennethcalamay.com>
2016-10-17 11:15:28 +0800

    Displays publishers inside ebay api key show

--- web/controllers/ebay_api_key_controller.ex
+++ web/controllers/ebay_api_key_controller.ex
@@ -4,6 +4,8 @@ defmodule Apientry.EbayApiKeyController do
   alias Apientry.Geo
   alias Apientry.Account
   alias Apientry.EbayApiKey
+  alias Apientry.TrackingId
+  alias Apientry.PublisherApiKey
 
   def index(conn, %{"account_id" => account_id}) do
     account = Repo.get(Account, account_id) |> Repo.preload(:geo)
@@ -40,8 +42,9 @@ defmodule Apientry.EbayApiKeyController do
     account = Repo.get(Account, account_id)
     geo = Repo.get(Geo, account.geo_id)
     ebay_api_key = Repo.get!(EbayApiKey, id)
-    # TODO: query publishers that use this ebay api key
-    render(conn, "show.html", ebay_api_key: ebay_api_key, account: account, geo: geo)
+    publisher_api_key_ids = from(t in TrackingId, where: t.ebay_api_key_id==^ebay_api_key.id, distinct: true, select: t.publisher_api_key_id) |> Repo.all
+    publisher_api_keys = from(p in PublisherApiKey, where: p.id in ^publisher_api_key_ids) |> Repo.all |> Repo.preload(:publisher)
+    render(conn, "show.html", ebay_api_key: ebay_api_key, account: account, geo: geo, publisher_api_keys: publisher_api_keys)
   end
 
   def edit(conn, %{"id" => id}) do
