ea6f02367d380a7b1fe7bc99d64c38a346ece949
KL <git@kennethcalamay.com>
2016-10-20 22:37:47 +0800

    Adds loader.io verification for new blackswan account

--- web/controllers/loader_io_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule Apientry.LoaderIoController do
-  use Apientry.Web, :controller
-
-  def index(conn, %{}) do
-    text(conn, "loaderio-ed96d28cb6cb372defb0748d98689a27")
-  end
-end
--- web/controllers/loader_verification_controller.ex
+++ web/controllers/loader_verification_controller.ex
@@ -12,4 +12,12 @@ defmodule Apientry.LoaderVerificationController do
   def show_694461ab(conn, _params) do
     conn |> text("loaderio-694461ab4a923d21b45b975c4032b9ff")
   end
+
+  def show_ed96d28c(conn, _params) do
+    conn |> text("loaderio-ed96d28cb6cb372defb0748d98689a27")
+  end
+
+  def show_7554f288(conn, _params) do
+    conn |> text("loaderio-7554f28861c6c058e54aaa267c439680")
+  end
 end
