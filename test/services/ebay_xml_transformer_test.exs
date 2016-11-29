a1377ab5e3d31c9b13194bf22d79b5cc5ab9c4a8
KL <git@kennethcalamay.com>
2016-11-29 14:10:28 +0800

    Adds tests for array of strings

--- test/services/ebay_xml_transformer_test.exs
+++ test/services/ebay_xml_transformer_test.exs
@@ -14,4 +14,10 @@ defmodule Apientry.EbayXmlTransformerTest do
     result = EbayXmlTransformer.transform(json)
     assert result == {"result", %{}, [{"keys", %{type: "array"}, [{"key", %{}, "value"}]}]}
   end
+
+  test "transforms an array of strings" do
+    json = %{"upc" => ["1234567890", "0987654321"]}
+    result = EbayXmlTransformer.transform(json)
+    assert result == {"result", %{}, [{"upc", %{type: "array"}, [{"upc", %{}, "1234567890"}, {"upc", %{}, "0987654321"}]}]}
+  end
 end
