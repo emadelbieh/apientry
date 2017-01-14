7b690f9b7836c46a93f9e1cfa97bd4b39f770063
KL <kl.calamay@blackswan.ventures>
2017-01-14 18:40:41 +0800

    point sandbox to ebay production

--- config/prod.exs
+++ config/prod.exs
@@ -25,8 +25,9 @@ config :apientry, Apientry.Repo,
 
 config :apientry, :db_cache, interval: 30_000
 
+# "http://sandbox.api.ebaycommercenetwork.com"
 config :apientry, :ebay_search_domain,
-  "http://sandbox.api.ebaycommercenetwork.com"
+  "http://api.ebaycommercenetwork.com"
 
 config :rollbax,
   access_token: "fcbe67e9abd04a69b3581fd26062c928",
