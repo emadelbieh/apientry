a24e6c089955f48cf34f806d7e2484c7befbf8f6
KL <kl.calamay@blackswan.ventures>
2017-01-14 08:56:04 +0800

    Load attrs from csv with stores removed

--- web/services/helpers.ex
+++ web/services/helpers.ex
@@ -1,4 +1,16 @@
 defmodule Apientry.Helpers do
   def load_ebay_data do
+    File.stream!("web/services/ebaydataus.csv")
+    |> CSV.decode()
+    |> Stream.map(fn row ->
+      downcased = Stream.map(row, &String.downcase/1)
+
+      [:cat_id, :cat_name, :attribute_name, :attribute_value_name, :attribute_value_id]
+      |> Stream.zip(downcased)
+      |> Enum.into(%{})
+    end)
+    |> Enum.reject(fn data ->
+        data.attribute_name in ["store", "fourchette de prix", "magasin", "online shop"]
+    end)
   end
 end
