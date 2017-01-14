e1dc9076589ccaf96d48390ecae92c8c493bba7d
KL <kl.calamay@blackswan.ventures>
2017-01-14 10:20:14 +0800

    Translates creation of attribute tree

--- web/services/helpers.ex
+++ web/services/helpers.ex
@@ -9,8 +9,36 @@ defmodule Apientry.Helpers do
       |> Stream.zip(downcased)
       |> Enum.into(%{})
     end)
-    |> Enum.reject(fn data ->
-        data.attribute_name in ["store", "fourchette de prix", "magasin", "online shop"]
+    # normalize data here, see helpers.js 142-147
+    |> Stream.reject(fn data ->
+      #data.attribute_name in ["store", "fourchette de prix", "magasin", "online shop"]
+      data.attribute_name == "store"
+    end)
+  end
+
+  def create_attr_tree(ebay_data) do
+    ebay_data
+    |> Enum.reduce(%{}, fn data, acc ->
+      attribute_value_name = data.attribute_value_name
+      attribute_value_name = String.replace(attribute_value_name, ~r/ *\([^)]*\) */, "")
+      attribute_value_name = String.replace(attribute_value_name, ~r/[|\\{}()[\]^$+*?.]/, "\\$&")
+      attribute_value_name = "\\b#{attribute_value_name}\\b"
+
+      if acc[data.cat_id] do
+        list = acc[data.cat_id]
+        list = [attribute_value_name | list]
+        Map.put(acc, data.cat_id, list)
+      else
+        Map.put(acc, data.cat_id, [attribute_value_name])
+      end
+    end)
+  end
+
+  def regex_from_attr_tree(attr_tree) do
+    Enum.reduce(attr_tree, %{}, fn {cat_id, attr_name_list}, acc ->
+      regex = Enum.join(attr_name_list, "|")
+      regex = "(#{regex})"
+      Map.put(acc, cat_id, regex)
     end)
   end
 end
