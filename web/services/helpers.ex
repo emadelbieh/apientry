d0333b06cd2370eb126e5a1b94031940106e1022
KL <kl.calamay@blackswan.ventures>
2017-01-24 09:22:11 +0800

    Uses path module to load the CSV

--- web/services/helpers.ex
+++ web/services/helpers.ex
@@ -1,7 +1,11 @@
 defmodule Apientry.Helpers do
   def regex_strings do
-    File.stream!("web/services/us_cat_id_attr_regex.csv")
+    Path.expand(".")
+    |> Path.join("/web/services/us_cat_id_attr_regex.csv")
+    |> File.stream!()
     |> CSV.decode()
-    |> Enum.reduce(%{}, fn [cat_id, regex], acc -> Map.put(acc, cat_id, regex) end)
+    |> Enum.reduce(%{}, fn [cat_id, regex], acc ->
+      Map.put(acc, cat_id, regex)
+    end)
   end
 end
