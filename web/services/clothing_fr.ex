c6b83e58f64de3eada69366286f13c5a4e9888ed
KL <kl.calamay@blackswan.ventures>
2017-01-25 11:15:25 +0800

    Translates laptops.js

--- web/services/clothing_fr.ex
+++ web/services/clothing_fr.ex
@@ -3,6 +3,28 @@ defmodule Apientry.ClothingFR do
 
   @rules ~w[Chaussette, Cardigan, Cardigans, Tunique, Tuniques, Sweatshirts, chaussettes, Vêtements, Vêtement, T-Shirts, T-Shirt, T Shirts, T Shirt, Chemises, Manteaux, Pyjama, Pyjamas, blouse, Denim, Legging, Manteau, veste, vestes, Maillot de bain, jupe, pull col, pantalon, pantalons, Robe, Robes, jupes]
 
+  @strong_attributes %{
+           "24347218_chaussettes" => ['chaussette', 'chaussettes'],
+                     "chemisiers" => ['chemisier', 'Chemise', 'Chemises'],
+                       "tuniques" => ['tunique'],
+    "24347218_manteaux_et_vestes" => ['manteaux','MANTEAU'],
+           "24347217_sweatshirts" => ['sweatshirt'],
+                 "24347218_jupes" => ['jupes'],
+             "24347218_pantalons" => ['pantalons'],
+  }
+
+  def category_id do
+    @category_id
+  end
+
+  def rules do
+    @rules
+  end
+
+  def strong_attributes do
+    @strong_attributes
+  end
+
   def genders do
     %{
         "enfants" => "24347227_enfants",
@@ -10,14 +32,4 @@ defmodule Apientry.ClothingFR do
         "femme"   => "24347227_femmes"
     }
   end
-
-    var strongattributes = {
-        "24347218_chaussettes": ['chaussette', 'chaussettes'],
-        "chemisiers": ['chemisier', 'Chemise', 'Chemises'],
-        "tuniques": ['tunique'],
-        "24347218_manteaux_et_vestes": ['manteaux','MANTEAU'],
-        "24347217_sweatshirts": ['sweatshirt'],
-        "24347218_jupes": ['jupes'],
-        "24347218_pantalons": ['pantalons']
-    };
 end
--- web/services/laptops_us.ex
+++ web/services/laptops_us.ex
@@ -1,3 +1,19 @@
 defmodule Apientry.LaptopsUS do
   @category_id "9007"
+
+  @attribute_values []
+
+  @rules ~r/(\b\d{1,3}gb\b|\b\d{1,3}tb\b|\bcore i|ssd|windows 10)/
+
+  @regex ~r/laptop|macbook|notebook/
+
+  def process(data) do
+    title = data.keywords
+
+    if String.length(title) > 5 && title =~ @regex && title =~ @rules do
+      Map.put(data, :has_match, true)
+    else
+      data
+    end
+  end
 end
