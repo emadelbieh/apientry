ab32b564b2ebbc6b1296e97433f0fe3d0201d513
KL <kl.calamay@blackswan.ventures>
2017-01-27 11:04:09 +0800

    Fixes file lookup

--- web/services/helpers.ex
+++ web/services/helpers.ex
@@ -1,11 +1,22 @@
 defmodule Apientry.Helpers do
   def regex_strings(geo) do
-    Path.expand(".")
-    |> Path.join("/web/services/#{geo}_cat_id_attr_regex.csv")
-    |> File.stream!()
-    |> CSV.decode()
-    |> Enum.reduce(%{}, fn [cat_id, regex], acc ->
-      Map.put(acc, cat_id, regex)
-    end)
+    try do
+      Path.expand(".")
+      |> Path.join("/web/services/#{geo}_cat_id_attr_regex.csv")
+      |> File.stream!()
+      |> CSV.decode()
+      |> Enum.reduce(%{}, fn [cat_id, regex], acc ->
+        Map.put(acc, cat_id, regex)
+      end)
+    rescue
+      File.Error ->
+        Path.expand("~")
+        |> Path.join("/#{geo}_cat_id_attr_regex.csv")
+        |> File.stream!()
+        |> CSV.decode()
+        |> Enum.reduce(%{}, fn [cat_id, regex], acc ->
+          Map.put(acc, cat_id, regex)
+        end)
+    end
   end
 end
