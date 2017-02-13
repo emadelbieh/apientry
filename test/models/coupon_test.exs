8e811902dc20e4486628a0a22d44f449ce93f3b3
KL <kl.calamay@blackswan.ventures>
2017-02-13 17:28:35 +0800

    Creates coupon model

---
+++ test/models/coupon_test.exs
@@ -0,0 +1,18 @@
+defmodule Apientry.CouponTest do
+  use Apientry.ModelCase
+
+  alias Apientry.Coupon
+
+  @valid_attrs %{category: "some content", code: "some content", country: "some content", dealtype: "some content", domain: "some content", enddate: "some content", holiday: "some content", lastmodified: "some content", logo: "some content", merchant: "some content", merchant_id: "some content", network: "some content", offer: "some content", rating: "some content", restriction: "some content", startdate: "some content", url: "some content", website: "some content"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = Coupon.changeset(%Coupon{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = Coupon.changeset(%Coupon{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
---
+++ web/models/coupon.ex
@@ -0,0 +1,35 @@
+defmodule Apientry.Coupon do
+  use Apientry.Web, :model
+
+  schema "coupons" do
+    field :merchant, :string
+    field :merchantid, :string
+    field :offer, :string
+    field :restriction, :string
+    field :url, :string
+    field :code, :string
+    field :startdate, :string
+    field :enddate, :string
+    field :category, :string
+    field :dealtype, :string
+    field :holiday, :string
+    field :network, :string
+    field :rating, :string
+    field :country, :string
+    field :logo, :string
+    field :website, :string
+    field :domain, :string
+    field :lastmodified, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:id, :merchant, :merchantid, :offer, :restriction, :url, :code, :startdate, :enddate, :category, :dealtype, :holiday, :network, :rating, :country, :logo, :website, :domain, :lastmodified])
+    |> validate_required([:id, :merchant, :merchantid, :offer, :restriction, :url, :code, :startdate, :enddate, :category, :dealtype, :holiday, :network, :rating, :country, :logo, :website, :domain, :lastmodified])
+  end
+end
