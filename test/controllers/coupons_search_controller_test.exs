48896300b844df3445e53412073b59d8b188b3d8
KL <kl.calamay@blackswan.ventures>
2017-02-17 12:41:09 +0800

    Fixes coupons search controller test

--- test/controllers/coupons_search_controller_test.exs
+++ test/controllers/coupons_search_controller_test.exs
@@ -2,6 +2,8 @@ defmodule Apientry.CouponSearchControllerTest do
   use Apientry.ConnCase, async: true
 
   alias Apientry.Coupon
+  alias Apientry.Publisher
+  alias Apientry.PublisherSubId
 
   @apientry %{id: "1", category: "some content", code: "some content", country: "some content", dealtype: "some content", domain: "apientry.com", enddate: "some content", holiday: "some content", lastmodified: "some content", logo: "some content", merchant: "some content", merchantid: "some content", network: "some content", offer: "some content", rating: "some content", restriction: "some content", startdate: "some content", url: "some content", website: "some content"}
   @apientry_with_category %{id: "2", category: "gift", code: "some content", country: "some content", dealtype: "some content", domain: "apientry.com", enddate: "some content", holiday: "some content", lastmodified: "some content", logo: "some content", merchant: "some content", merchantid: "some content", network: "some content", offer: "some content", rating: "some content", restriction: "some content", startdate: "some content", url: "some content", website: "some content"}
@@ -12,6 +14,9 @@ defmodule Apientry.CouponSearchControllerTest do
 
 
   setup do
+    {:ok, publisher} = Repo.insert(Publisher.changeset(%Publisher{}, %{name: "PubName"}))
+    Repo.insert(PublisherSubId.changeset(%PublisherSubId{}, %{publisher_id: publisher.id, sub_id: "001"}))
+
     changeset = Coupon.changeset(%Coupon{}, @apientry)
     {:ok, coupon} = Repo.insert(changeset)
 
@@ -34,41 +39,41 @@ defmodule Apientry.CouponSearchControllerTest do
   end
 
   test "urls are in redirect format", %{conn: conn} do
-    conn = get conn, coupon_search_path(conn, :search, domain: "apientry.com")
+    conn = get conn, coupon_search_path(conn, :search, domain: "apientry.com", subid: "001")
     json = hd(json_response(conn, 200))
     assert json["url"] =~ ~r/redirect/
   end
 
   test "list entries by domain name", %{conn: conn} do
-    conn = get conn, coupon_search_path(conn, :search, domain: "apientry.com")
+    conn = get conn, coupon_search_path(conn, :search, domain: "apientry.com", subid: "001")
     coupons = json_response(conn, 200)
     assert Enum.count(coupons) == 5
     assert hd(coupons)["domain"] == "apientry.com"
   end
 
   test "list entries by domain name and category", %{conn: conn} do
-    conn = get conn, coupon_search_path(conn, :search, domain: "apientry.com", category: "gift")
+    conn = get conn, coupon_search_path(conn, :search, domain: "apientry.com", category: "gift", subid: "001")
     coupons = json_response(conn, 200)
     assert Enum.count(coupons) == 1
     assert hd(coupons)["category"] == "gift"
   end
 
   test "list entries by domain name and holiday", %{conn: conn} do
-    conn = get conn, coupon_search_path(conn, :search, domain: "apientry.com", holiday: "valentinesday")
+    conn = get conn, coupon_search_path(conn, :search, domain: "apientry.com", holiday: "valentinesday", subid: "001")
     coupons = json_response(conn, 200)
     assert Enum.count(coupons) == 1
     assert hd(coupons)["holiday"] == "valentinesday"
   end
 
   test "list entries by domain name and network", %{conn: conn} do
-    conn = get conn, coupon_search_path(conn, :search, domain: "apientry.com", network: "cj")
+    conn = get conn, coupon_search_path(conn, :search, domain: "apientry.com", network: "cj", subid: "001")
     coupons = json_response(conn, 200)
     assert Enum.count(coupons) == 1
     assert hd(coupons)["network"] == "cj"
   end
 
   test "list entries by domain name and dealtype", %{conn: conn} do
-    conn = get conn, coupon_search_path(conn, :search, domain: "apientry.com", dealtype: "coupon")
+    conn = get conn, coupon_search_path(conn, :search, domain: "apientry.com", dealtype: "coupon", subid: "001")
     coupons = json_response(conn, 200)
     assert Enum.count(coupons) == 1
     assert hd(coupons)["dealtype"] == "coupon"
