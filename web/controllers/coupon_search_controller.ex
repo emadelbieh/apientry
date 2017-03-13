13a96acf8a48f999f3e0b7524c16f98c6be1954a
KL <kl.calamay@blackswan.ventures>
2017-03-13 12:31:13 +0800

    Filter coupons by country by default

--- web/controllers/coupon_search_controller.ex
+++ web/controllers/coupon_search_controller.ex
@@ -8,7 +8,28 @@ defmodule Apientry.CouponSearchController do
     assigns = conn.assigns
     assigns = Map.put(assigns, :redirect_base, "#{conn.scheme}://#{conn.host}:#{conn.port}/redirect/")
     conn = Map.put(conn, :assigns, assigns)
+
+    country = conn.params["country"] || get_country(conn.params)
+    params = Map.put(conn.params, "country", country)
+    conn = Map.put(conn, :params, params)
+
     coupons = Coupon.by_params(conn)
     json conn, Coupon.to_map(coupons)
+  end
+
+  defp get_country(%{"visitorIPAddress" => ip}) do
+    case IpLookup.lookup(ip) do
+      nil -> {:error, :unknown_country, %{ip: ip}}
+      country ->
+        if country == "GB" do
+          "UK"
+        else
+          country
+        end
+    end
+  end
+
+  defp get_country(_) do
+    "US"
   end
 end
