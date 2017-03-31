ba6a377feb10123bd6593350ba9db27695aaf7d5
KL <kl.calamay@blackswan.ventures>
2017-03-31 14:23:14 +0800

    Allow modification of reference data for publisher sub id

--- web/models/publisher_sub_id.ex
+++ web/models/publisher_sub_id.ex
@@ -14,7 +14,7 @@ defmodule Apientry.PublisherSubId do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:sub_id, :publisher_id])
+    |> cast(params, [:sub_id, :publisher_id, :reference_data])
     |> validate_required([:sub_id])
   end
 end
