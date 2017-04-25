dd00619d3ce84d10b7846b8071f9aaf154e37a4a
KL <kl.calamay@blackswan.ventures>
2017-04-25 19:09:09 +0800

    Displays cloudflare request headers

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,20 @@ defmodule Apientry.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  @doc """
+  For testing cloudflare values
+  """
+  def cloudflare_values(conn, _params) do
+    map = %{
+      country: get_req_header(conn, "cf-ipcountry"),
+      ip: get_req_header(conn, "cf-connecting-ip"),
+      fowarded_for: get_req_header(conn, "x-forwarded-for"),
+      fowarded_proto: get_req_header(conn, "x-forwarded-proto"),
+      cf_ray: get_req_header(conn, "cf-ray"),
+      cf_visitor: get_req_header(conn, "cf-visitor")
+    }
+
+    json(conn, map)
+  end
 end
