dbbb2998a96f50d3f34f88dd901e5287f1be2dc5
KL <kl.calamay@blackswan.ventures>
2017-05-01 12:13:20 +0800

    Download feed to file before processing

--- web/services/download_coupon_worker.ex
+++ web/services/download_coupon_worker.ex
@@ -1,45 +1,69 @@
 defmodule Apientry.DownloadCouponWorker do
-  @endpoint "https://api.feeds4.com/coupons/?token=bl213euxg-scof-zq44-f3b4589h74&recordset=all&format=json"
+  @cache_path "hello"
+  @endpoint "https://api.feeds4.com/coupons/?token=bl213euxg-scof-zq44-f3b4589h74&recordset=test&format=json"
 
   alias HTTPoison.Response
   alias Apientry.Coupon
   alias Apientry.Repo
-  alias Apientry.Reponse
 
   def perform do
+    @endpoint
+    |> query()
+    |> save_to_file(@cache_path)
+
+    @cache_path
+    |> read_contents()
+    |> parse_contents()
+    |> cache_to_database()
+  end
+
+  defp query(endpoint) do
     case HTTPoison.get(@endpoint) do
-      {:ok,  %Response{status_code: status, body: body, headers: headers}} ->
-        body = Poison.decode!(body)
-
-        Repo.delete_all(Coupon)
-
-        body["data"]
-        |> Stream.filter(fn coupon -> coupon["country"] == "US" end)
-        |> Enum.each(fn coupon ->
-          if(db_coupon = Repo.get(Coupon, coupon["id"])) do
-            coupon = Map.delete(coupon, "id")
-            changeset = Coupon.changeset(db_coupon, coupon)
-            case Repo.update(changeset) do
-              {:ok, coupon} ->
-                IO.puts("Coupon #{coupon.id} successfully updated")
-              {:error, changeset} ->
-                IO.puts("Error saving coupon:")
-                IO.inspect changeset
-            end
-          else
-            changeset = Coupon.changeset(%Coupon{}, coupon)
-            case Repo.insert(changeset) do
-              {:ok, coupon} ->
-                IO.puts("Coupon #{coupon.id} successfully saved")
-              {:error, changeset} ->
-                IO.puts("Error saving coupon:")
-                IO.inspect changeset
-            end
-          end
-        end)
-
+      {:ok,  %Response{status_code: status, body: body, headers: headers} = response} ->
+        body
       {:error, %HTTPoison.Error{reason: reason} = error} ->
-        IO.puts "error"
+        IO.inspect(error)
+        nil
+    end
+  end
+
+  defp save_to_file(data, cache_path) do
+    case File.open(cache_path, [:write]) do
+      {:ok, file} ->
+        IO.binwrite file, data
+        File.close(file)
+        true
+      _ ->
+        false
+    end
+  end
+
+  def read_contents(cache_path) do
+    case File.read(cache_path) do
+      {:ok, data} -> data
+      _ -> %{}
+    end
+  end
+
+  defp parse_contents(response) do
+    Poison.decode!(response)["data"]
+  end
+
+  defp cache_to_database(coupons_data) do
+    Repo.delete_all(Coupon)
+    Enum.each(coupons_data, fn coupon_attrs ->
+      create_record(coupon_attrs)
+    end)
+  end
+
+  defp create_record(coupon_attrs) do
+    changeset = Coupon.changeset(%Coupon{}, coupon_attrs)
+    case Repo.insert(changeset) do
+      {:ok, coupon} ->
+        IO.puts("Coupon #{coupon.id} successfully saved")
+      {:error, changeset} ->
+        IO.puts("Error saving coupon:")
+        IO.inspect changeset
     end
   end
 end
