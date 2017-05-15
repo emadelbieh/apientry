f881f2372f88fd680eccac2da5acdb25fc15f955
KL <kl.calamay@blackswan.ventures>
2017-05-15 08:40:21 +0800

    Run coupon copies 35 minutes past every 6hrs

--- config/prod.aws.exs
+++ config/prod.aws.exs
@@ -32,7 +32,7 @@ config :quantum, :apientry,
   cron: [
       "* */6 * * *": {"Timex", :now},
       "5 */6 * * *": {"Apientry.DownloadCouponWorker", :perform},
-      "31 */6 * * *": {"Apientry.DownloadCouponCopyWorker", :perform},
+      "35 */6 * * *": {"Apientry.DownloadCouponCopyWorker", :perform},
   ]
 
 # Load remotely in AWS, because the local .mmdb file is not available when
--- web/models/coupon.ex
+++ web/models/coupon.ex
@@ -49,7 +49,7 @@ defmodule Apientry.Coupon do
 
   def base_model do
     {_, {hour, minute, _}} = Timex.to_erl(Timex.now())
-    if (hour in [0,6,12,18]) && (minute in 0.30) do
+    if (hour in [0,6,12,18]) && (minute in 0..30) do
       Apientry.CouponCopy
     else
       Apientry.Coupon
