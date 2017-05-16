d80f19a6226acc79d9abd691fb10f8f3b5b8f381
KL <kl.calamay@blackswan.ventures>
2017-05-16 11:26:21 +0800

    Adds title cleaner

---
+++ test/services/title_cleaner_test.exs
@@ -0,0 +1,25 @@
+defmodule Apientry.TitleCleanerTest do
+  use ExUnit.Case
+
+  alias Apientry.TitleCleaner, as: TC
+
+  test "clean removes ellipses" do
+    assert TC.clean("This is ... a keyword") == "this is a keyword"
+  end
+
+  test "clean removes dashes" do
+    assert TC.clean("This is - a keyword") == "this is a keyword"
+  end
+
+  test "clean removes slashes" do
+    assert TC.clean("This is / a / keyword") == "this is a keyword"
+  end
+
+  test "clean collapses multiple spaces" do
+    assert TC.clean("This   is    a  keyword") == "this is a keyword"
+  end
+
+  test "clean removes stopwords" do
+    assert TC.clean("This avec offres spéciales is fiyatı a keyword undefined") == "this is a keyword "
+  end
+end
---
+++ web/services/title_cleaner.ex
@@ -0,0 +1,46 @@
+defmodule Apientry.TitleCleaner do
+  @stopwords [
+    "avec offres spéciales",
+    "peter hahn",
+    "fiyatı",
+    "undefined",
+    "offres spéciales",
+    "la fiche produit"
+  ]
+
+  def clean(title) do
+    title
+    |> String.downcase()
+    |> ellipses_to_spaces()
+    |> dashes_to_spaces()
+    |> slashes_to_spaces()
+    |> remove_stop_words()
+    |> multiple_to_single_spaces()
+  end
+
+  defp ellipses_to_spaces(string) do
+    string
+    |> String.replace("...", " ")
+  end
+
+  defp dashes_to_spaces(string) do
+    string
+    |> String.replace("-", " ")
+  end
+
+  defp slashes_to_spaces(string) do
+    string
+    |> String.replace("/", " ")
+  end
+
+  defp multiple_to_single_spaces(string) do
+    string
+    |> String.replace(~r/ +(?= )/, "")
+  end
+
+  def remove_stop_words(string) do
+    Enum.reduce(@stopwords, string, fn stopword, acc ->
+      String.replace(acc, stopword, "")
+    end)
+  end
+end
