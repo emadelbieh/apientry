22fd999552dae292a31fc3d99d08050575e60f8f
KL <kl.calamay@blackswan.ventures>
2017-05-20 09:47:55 +0800

    Adds subplacements to publishers endpoint

--- web/views/intern/publisher_view.ex
+++ web/views/intern/publisher_view.ex
@@ -13,6 +13,20 @@ defmodule Apientry.Intern.PublisherView do
     %{id: publisher.id,
       name: publisher.name,
       revenue_share: publisher.revenue_share,
-      report_receivers: publisher.report_receivers}
+      receivers: publisher.report_receivers,
+      subplacements: subplacements_for(publisher)}
+  end
+
+  defp subplacements_for(publisher) do
+    publisher.api_keys
+    |> Enum.flat_map(fn api_key ->
+      api_key.tracking_ids
+      |> Enum.map(fn tracking_id ->
+        %{
+          subplacement: tracking_id.subplacement,
+          tracking_id: tracking_id.code,
+        }
+      end)
+    end)
   end
 end
