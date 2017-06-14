4d67f7de71be38bd664b6f555431527797e5df3c
KL <kl.calamay@blackswan.ventures>
2017-06-14 17:03:50 +0800

    Remove sizes for dedup

--- web/services/title_filter.ex
+++ web/services/title_filter.ex
@@ -1,6 +1,24 @@
 defmodule Apientry.TitleFilter do
   alias Apientry.TitleAgent
 
+  def remove_sizes(body) do
+    categories = Enum.map(get_categories(body), fn category ->
+      items = Enum.map(get_items(category), fn item ->
+        name = String.replace(get_name(item), ~r/size:\s*\d+\.*\d*,\s*/i, "")
+
+        if item["offer"] do
+          update_in(item["offer"]["name"], fn _ -> name end)
+        else
+          update_in(item["product"]["name"], fn _ -> name end)
+        end
+      end)
+
+      update_in(category["items"]["item"], fn _ -> items end)
+    end)
+
+    update_in(body["categories"]["category"], fn _ -> categories end)
+  end
+
   def filter_duplicate_title(body) do
     {ok, titles} = TitleAgent.start_link
 
