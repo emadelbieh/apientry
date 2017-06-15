67ec2416e70ed57c1ea3069e9e8a17e0f0ff8fee
KL <kl.calamay@blackswan.ventures>
2017-06-15 19:33:35 +0800

    Turn auth into a plug

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,17 +1,14 @@
 defmodule Apientry.UserController do
   use Apientry.Web, :controller
 
+  plug :authenticate when action in [:index]
+
   def index(conn, _params) do
-    case authenticate(conn) do
-      %Plug.Conn{halted: true} = conn ->
-        conn
-      conn ->
-        users = Repo.all(User)
-        render conn, "index.html", users: users
-    end
+    users = Repo.all(User)
+    render conn, "index.html", users: users
   end
 
-  def authenticate(conn) do
+  def authenticate(conn, _opts) do
     if conn.assigns.current_user do
       conn
     else
