1a24c1835050bc2c8e0665948c92f79eddb9bba7
KL <kl.calamay@blackswan.ventures>
2017-06-16 10:25:19 +0800

    Makes all tests passing

--- config/config.exs
+++ config/config.exs
@@ -49,7 +49,7 @@ config :apientry, :events,
   url: "https://bsevents.ngrok.io"
 
 config :apientry, :ebay_search_domain,
-  "http://sandbox.api.ebaycommercenetwork.com"
+  "http://api.ebaycommercenetwork.com"
 
 # access_token always needs to be set, even if we're not using Rollbar.
 config :rollbax,
