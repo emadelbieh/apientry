b3f3fc37786239bd64a23d8bc551a16102bd9b37
KL <kl.calamay@blackswan.ventures>
2017-06-21 21:31:00 +0800

    Finishes auth

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -54,7 +54,7 @@ defmodule Apientry.Auth do
     else
       conn
       |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: Helpers.page_path(conn, :index))
+      |> redirect(to: Helpers.session_path(conn, :new))
       |> halt()
     end
   end
