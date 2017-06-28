b5b60a6fc8137c60ee6f6a79406e308e7d548686
Gabriel Jaldon <gjaldon85@gmail.com>
2017-06-29 03:00:34 +0800

    Use render to format data in query action

--- web/controllers/publisher_sub_id_controller.ex
+++ web/controllers/publisher_sub_id_controller.ex
@@ -74,16 +74,6 @@ defmodule Apientry.PublisherSubIdController do
 
   def query(conn, params) do
     subids = Repo.all(from p in PublisherSubId, where: p.visual_search == ^true)
-    json(conn, %{subids: format_subids(subids)})
-  end
-
-  def format_subids(subids) do
-    subids
-    |> Enum.map(fn subid ->
-      %{
-        sub_id: subid.sub_id,
-        reference_data: subid.reference_data
-      }
-    end)
+    render(conn, "query.json", %{subids: subids})
   end
 end
--- web/views/publisher_sub_id_view.ex
+++ web/views/publisher_sub_id_view.ex
@@ -1,3 +1,14 @@
 defmodule Apientry.PublisherSubIdView do
   use Apientry.Web, :view
+
+
+  def render("query.json", %{subids: subids}) do
+    subids = subids
+    |> Enum.map(fn subid ->
+      %{sub_id: subid.sub_id,
+        reference_data: subid.reference_data}
+    end)
+
+    %{subids: subids}
+  end
 end
