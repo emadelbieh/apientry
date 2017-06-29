02f10582a661efb208efe3c833f702b3ecfb88a1
Gabriel Jaldon <gjaldon85@gmail.com>
2017-06-29 16:57:36 +0800

    Move CSV files to priv directory

--- web/services/helpers.ex
+++ web/services/helpers.ex
@@ -18,30 +18,13 @@ defmodule Apientry.Helpers do
   end
 
   def load_regexes_from_file(cache, geo) when geo in ~w(fr us au de gb) do
-    try do
-      Path.expand(".")
-      |> Path.join("/web/services/#{geo}_cat_id_attr_regex.csv")
-      |> File.stream!()
-      |> CSV.decode()
-      |> Enum.each(fn [cat_id, regex] ->
-        CsvCache.put(cache, cat_id, regex)
-      end)
-      #|> Enum.reduce(%{}, fn [cat_id, regex], acc ->
-      #  Map.put(acc, cat_id, regex)
-      #end)
-    rescue
-      File.Error ->
-        Path.expand("~")
-        |> Path.join("/#{geo}_cat_id_attr_regex.csv")
-        |> File.stream!()
-        |> CSV.decode()
-        |> Enum.each(fn [cat_id, regex] ->
-          CsvCache.put(cache, cat_id, regex)
-        end)
-        #|> Enum.reduce(%{}, fn [cat_id, regex], acc ->
-        #  Map.put(acc, cat_id, regex)
-        #end)
-    end
+    :code.priv_dir(:apientry)
+    |> Path.join("regexes/#{geo}_cat_id_attr_regex.csv")
+    |> File.stream!()
+    |> CSV.decode()
+    |> Enum.each(fn [cat_id, regex] ->
+      CsvCache.put(cache, cat_id, regex)
+    end)
   end
 
   def load_regexes_from_file(_, _) do
